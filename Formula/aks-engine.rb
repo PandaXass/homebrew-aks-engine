110ca7fb11a44cdb11bb89615182e8a31e575745
Wenlan Yang <wenlan.yang@mojang.com>
2019-02-20 13:30:03 +0100

    Update to aks-engine 0.31.1

--- Formula/aks-engine.rb
+++ Formula/aks-engine.rb
@@ -1,9 +1,9 @@
 class AksEngine < Formula
     desc "A tool to quickly bootstrap Kubernetes cluster on Azure"
     homepage "https://github.com/Azure/aks-engine"
-    version "0.28.1"
+    version "0.31.1"
     url "https://github.com/Azure/aks-engine/releases/download/v#{version}/aks-engine-v#{version}-darwin-amd64.tar.gz"
-    sha256 "5d41a836938db99dc15a2ffc8e7f17c05318947bb1746b5b60021410b156d623"
+    sha256 "c7120e5b5d09d8aa533b82412a87da4c939b6eb4719f7cc2ec08185abf880770"
 
     def install
       bin.install name
